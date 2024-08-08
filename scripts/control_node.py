a55cefd412a119b024adf9e03156de8a9143b583
Jolan-Van-Passel <jolan.vanpassel@gmail.com>
2024-08-08 16:11:03 +0200

    Add files via upload

--- scripts/control_node.py
+++ scripts/control_node.py
@@ -5,49 +5,62 @@ import rospy
 import time
 from geometry_msgs.msg import Vector3
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
-
-def control_callback(msg: Image):
-    global timeOfLastCallback
-    timeOfLastCallback = time.time_ns()//1000000
-    bridge = CvBridge()
-    image = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-    coordinates = getCoordinates(image)
-
-    if len(coordinates) == 4:
-        v = getCameraVelocity_4dots(image)
-    else:
-        if -1 <= int(getError_1dot(image)[0]) <= 1 and -1 <= int(getError_1dot(image)[1]) <= 1 and -10 <= int(getError_1dot(image)[2]) <= 10:
-            global goalReached
-            goalReached = True
-        v = getCameraVelocity_1dot(image)
-    pub.publish(v)
+import cv2 as cv
+from cv_bridge import CvBridge   
 
 if __name__ == '__main__':
     rospy.init_node("control_node")
-    pub = rospy.Publisher("/velocity", Vector3, queue_size=10)
-    sub = rospy.Subscriber("/usb_cam/image_raw", Image, callback=control_callback)
-    rospy.loginfo("control node has been started")
+    pub = rospy.Publisher("/automation_velocity", Vector3, queue_size=20)
+    pub2 = rospy.Publisher("/camera/image", Image, queue_size=20)
+    cap = cv.VideoCapture(0, cv.CAP_V4L2)
+    if not cap.isOpened():
+        rospy.logerr("cannot open camera")
+        rospy.signal_shutdown("cannot open camera")
+    else:
+        rospy.loginfo("control node has been started")
 
     rate = rospy.Rate(30)
     v = Vector3()
-    timeOfLastCallback = time.time_ns()//1000000
     goalReached = False
-    
+    bridge = CvBridge()
+
     while not rospy.is_shutdown():
-        currentTime = time.time_ns()//1000000
-        if currentTime - timeOfLastCallback > 66:
+        ret, frame = cap.read()
+        coordinates = getCoordinates(frame)
+
+        if len(coordinates) == 4:
+            v = getCameraVelocity_4dots(frame)
+        else:
+            rospy.logdebug("test1")
+            try:
+                if -1 <= int(getError_1dot(frame)[0]) <= 1 and -1 <= int(getError_1dot(frame)[1]) <= 1 and -10 <= int(getError_1dot(frame)[2]) <= 10:
+                    goalReached = True
+            except:
+                rospy.loginfo("goal not found")
+                v.x = 0
+                v.y = 0
+                v.z = 0
+            v = getCameraVelocity_1dot(frame)
+        if not ret:
             v.x = 0
             v.y = 0
             v.z = 0
             pub.publish(v)
             rospy.loginfo("velocity set to zero due to lack of input")
-        if goalReached:
+        elif goalReached:
             v.x = 0
             v.y = 0
             v.z = 0
             rospy.loginfo("goal has been reached")
             pub.publish(v)
-            rospy.signal_shutdown("goal has been reached")
+            cap.release()
+            cv.destroyAllWindows()
+            rospy.signal_shutdown("control node has stopped")
+        else:
+            rospy.loginfo(str(v.x) + "\n" + str(v.y) + "\n" + str(v.z))
+            pub.publish(v)
+            image = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
+            image_message = bridge.cv2_to_imgmsg(frame, encoding="bgr8")
+            pub2.publish(image_message)
         rate.sleep()
     
